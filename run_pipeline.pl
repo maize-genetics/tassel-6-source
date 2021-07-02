26cc8a19d3c561b009f21855d75a057115db89c2
Terry Casstevens <tmc46@cornell.edu>
2021-07-02 09:03:24 -0400

    Add run_pipeline.pl

---
+++ run_pipeline.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl -w
+
+use strict;
+use File::Basename;
+
+my $top = dirname(__FILE__);
+$top //= '.';
+
+# Scan @ARGV for Java memory arguments, and put rest in @args
+my $java_mem_min_default = "-Xms512m";
+my $java_mem_max_default = "-Xmx1536m";
+my $java_mem_min = "";
+my $java_mem_max = "";
+my @args;
+for (my $i=0; $i<=$#ARGV; $i++){
+   if ($ARGV[$i] =~ m/Xms/) {
+      $java_mem_min .= "$ARGV[$i]";
+      $java_mem_min=~s/–/-/g;
+   }
+   elsif ($ARGV[$i] =~ m/Xmx/) {
+      $java_mem_max .= "$ARGV[$i]";
+      $java_mem_max=~s/–/-/g;
+   }
+   else{
+      push(@args, $ARGV[$i]);
+   }
+}
+
+if ($java_mem_min eq "") { $java_mem_min = $java_mem_min_default; }
+if ($java_mem_max eq "") { $java_mem_max = $java_mem_max_default; }
+
+$ENV{'GRADLE_OPTS'} = "$java_mem_min $java_mem_max";
+print "GRADLE_OPTS = $ENV{'GRADLE_OPTS'}\n";
+
+print "Memory Settings: $java_mem_min $java_mem_max\n";
+print "Tassel Pipeline Arguments: " . "@args\n";
+
+#system "java -classpath '$CP' $java_mem_min $java_mem_max net.maizegenetics.pipeline.TasselPipeline @args";
+
+system "time ./gradlew -q run --args='-debug @args'";
