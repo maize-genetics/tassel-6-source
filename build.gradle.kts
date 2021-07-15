ef9e5c5f74649481eae1dcf1e94c919de888d1ef
Terry Casstevens <tmc46@cornell.edu>
2021-07-14 20:22:46 -0400

    New Build Date

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 // Dependencies will follow the buildscript
 
 group = "net.maizegenetics"
-version = "6.0.0"
+version = "6.0.1"
 
 
 buildscript {
